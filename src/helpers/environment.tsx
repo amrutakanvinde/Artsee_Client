823b277bf861f3e43dc8821dd1510a77e253b4ec
Amruta Ray <amrutaray@Amrutas-MacBook-Air.local>
2020-10-13 11:49:50 -0400

    first heroku push

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -5,8 +5,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000';
         break;
-    case 'artsee-client.herokuapp.com/':
-        APIURL = 'https://artsee-server.herokuapp.com/'
+    case 'artsee-client.herokuapp.com':
+        APIURL = 'https://artsee-server.herokuapp.com'
 }
 
 export default APIURL;  
